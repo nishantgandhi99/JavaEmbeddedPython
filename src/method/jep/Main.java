e857985e53b01a2fb9dd559b0d016a523f4186e6
nishantgandhi99 <nishantgandhi99@gmail.com>
2016-03-08 15:51:28 +0530

    Python Script Runner

--- src/method/jep/Main.java
+++ src/method/jep/Main.java
@@ -4,8 +4,8 @@ import jep.Jep;
 import jep.JepException;
 
 public class Main {
-	public static void main(String[] args) throws JepException {
 
+	public static void testJepCall() throws JepException {
 		Jep jep = new Jep();
 		jep.eval("import sys");
 		jep.eval("s = 'Hello World'");
@@ -13,5 +13,17 @@ public class Main {
 		String java_string = jep.getValue("s").toString();
 		System.out.println("Java String:" + java_string);
 		jep.runScript("src/myscript.py");
+		jep.close();
+	}
+	public static void runPythonScript(String filepath) throws JepException
+	{
+		Jep jep = new Jep();
+		jep.runScript(filepath);
+		jep.close();
+	}
+	
+	public static void main(String[] args) throws JepException {
+		//testJepCall();
+		runPythonScript("src/NW.py");
 	}
 }
